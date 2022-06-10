3d324008fd07d4ce0838f038e285d09994030dcb
Fabio Cataleta <fabio.cataleta@caplin.com>
2022-06-10 17:11:04 +0100

    Removed unnecessary characters from game titles

--- sales_tracker.py
+++ sales_tracker.py
@@ -1,5 +1,7 @@
 from __future__ import print_function
 
+import re
+
 from google_doc_mapper import GoogleDocMapper
 
 # The ID of a sample document.
@@ -8,7 +10,7 @@ DOCUMENT_ID = '1EDpqzYphtaxSEcgQ6RBrkLehytIMaHePPSvxyO68Em0'
 
 class SalesTracker:
     def run(self):
-        ### Retrieve text from Google Doc, turn into array of games we are interested in ###
+        ### Get games we are interested in from Google Doc ###
         games_of_interest = self.getGamesFromDoc()
         print(games_of_interest)
 
@@ -25,7 +27,7 @@ class SalesTracker:
         ### Extension: Allow budget to be set for max game on sale can cost ###
 
     def getGamesFromDoc(self):
-        """Gets the list of 'To Play' games"""
+        """Gets the list of 'To Play' games from the Google Doc"""
 
         # Get the raw content from the Google Doc
         mapper = GoogleDocMapper()
@@ -35,11 +37,24 @@ class SalesTracker:
         all_paragraphs = [
             game for game in raw_content if "paragraph" in game]
 
-        [games_to_play, heading_indexes] = mapper.map_to_entries_and_headings(
+        [games, heading_indexes] = mapper.map_to_entries_and_headings(
             all_paragraphs)
 
         # Structure of doc means "To Play" games are always at the start
-        return games_to_play[heading_indexes[0] + 1: heading_indexes[1]]
+        games_to_play = games[heading_indexes[0] + 1: heading_indexes[1]]
+
+        # Remove all entries which are line breaks
+        games_to_play_no_whitespace = [
+            game for game in games_to_play if game != "\n"]
+
+        # Remove undesirable characters from game titles
+        sanitised_games_to_play = []
+        pattern = re.compile(r"\s\s*$")
+        for game in games_to_play_no_whitespace:
+            game_title = game.replace("(NYA)", "").replace("(NYR)", "")
+            sanitised_games_to_play.append(re.sub(pattern, "", game_title))
+
+        return sanitised_games_to_play
 
 
 sales_tracker = SalesTracker()
